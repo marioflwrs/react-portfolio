bbdcbbc2d5756f566942c55bf725c6e6d9d735e0
wawamario <mariomuloz@gmail.com>
2021-03-24 00:15:11 -0400

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <Router basepath={process.env.PUBLIC_URL}>
+      <Router>
         <Home exact path="/" />
         <Breaking path="/breaking" />
         <Projects path="/projects" />
