9a79a662de6f109f0b607606ca896f4300439a62
wawamario <mariomuloz@gmail.com>
2021-06-11 11:07:48 -0400

    rebuild

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import { Router } from "@reach/router";
-
 //Components
 import Home from './components/Home/Home';
-import Breaking from './components/Breaking/Breaking';
-import Projects from './components/Projects/Projects';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Home exact path="/" />
-        <Breaking path="/breaking" />
-        <Projects path="/projects" />
-      </Router>
+      <Home />
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,10 @@
-//Components
-import Navigation from '../Navigation/Navigation';
-
-//Style
 import './Home.scss';
-
-//Images
-import galogo from '../../assets/images/ga-logo.png';
-import inflectralogo from '../../assets/images/inflectra-logo.png';
 
 
 const Home = () => {
     return(
         <div className="home-container">
-            <Navigation />
-            <p>I'm a breakdancing web developer!<br/> I love to spin on my head and create fully functioning websites using React.</p>
-
-            <div className="skills-container">                
-                <div className="home-title-container">
-                    <h2><strong>Experience</strong></h2> 
-                </div>
-
-                <div className="section-container inflectra">
-                    <img src={inflectralogo} alt="inflectra logo" className="inflectra-logo"/>
-                    <p>React.JS Internship | Sept 2017 - November 2017</p>
-                </div>
-                
-                <div className="section-container">
-                    <img src={galogo} alt="General Assembly logo" className="ga-logo"/>
-                    <p>Front-End Web Development 2016 <br /> Bootcamp</p>
-                </div>
-            </div>           
+        <h1>Hello.</h1>
         </div>
     );
 }
