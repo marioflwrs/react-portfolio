5897f28bb678ee72e6b39de3dbe9c2de8c4f9608
wawamario <mariomuloz@gmail.com>
2021-06-15 00:35:24 -0400

    styling and content updated for both Landing, Work, and About components.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,9 +8,19 @@ const About = () => {
         <div className="about-container">
             <Logo />
             <div className="about-article-container">
-                <img src={mariosmile} alt="about avatar" />
-                <h1>title</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et illum eum id repellat sapiente eos quam ad, sint laborum quas vitae odio quasi esse aliquid suscipit ratione labore eligendi saepe!</p>
+                <div className="article-container-inner">
+                    <h1 >I build websites using today's latest practices</h1>
+                    <img src={mariosmile} alt="about avatar" />
+                    <h1>About</h1>
+                    <h2>Hello! I'm Mario. I'm a self-taught breakdancing web-developer with a total of 5+ years of web development experience.</h2>
+                    <p>Thats right! Just how I love to spin on my head, I also love to spin my code into fully functioning web applications.</p>
+                    <p>What initially started out as a hobby to help out some friends with the structuring & styling for their websites, soon became a hidden passion that I began to explore.</p>
+                    <p>My journey started in 2016 when I attended General Assembly's Web Development bootcamp course. I solidified my basic knowledge of html, css, sass, bootstrap, javascript, jquery, and then continued on to learn more about newer technologies being used in the webdev industry such as: React, Node, Express, Axios, REST API's, MongoDB, Heroku.</p>
+                    <p>As a person, I am a natural introvert with an abundance of curiosity. I'm always looking for new ways to be able to solve bugs along with providing reuable code. </p>
+                    <p>I currently work remotely creating websites with a selected freelance clientele base and am open for new opportunities.</p>
+
+                    <a href="https://drive.google.com/file/d/1AA7DD9LNsOjLRVjI0Had843N0zhqCRbr/view?usp=sharing" target="_blank" rel="noreferrer"><h2 className="resume-style">resume.</h2></a>
+                </div>
             </div>
         </div>
     );
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,8 +8,8 @@ const Landing = () => {
             <div className="landing-nav-container">
                 <Link to="/work"><h1>Work</h1></Link>
                 <Link to="/about"><h1>About</h1></Link>
-                <Link to="/contact"><h1>Contact</h1></Link>
                 <Link to="/breaking"><h1>Breakin'</h1></Link>
+                <Link to="/contact"><h1>Contact</h1></Link>
             </div>
         </div>
     );
