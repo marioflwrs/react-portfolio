18401046345e734f9b45f6309af02fe1cd81b6a2
wawamario <mariomuloz@gmail.com>
2021-06-16 14:25:11 -0400

    contact styling has been updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,9 @@
-import Logo from "../Logo/Logo";
 import './Contact.scss';
 
 const Contact = () => {
     return(
         <div className="contact-container">
-            <Logo />
-            <div className="inner-contact-container">
+            <div className="contact-inner-container">
                 <h1>Contact.</h1>
                 <p><a href="https://www.linkedin.com/in/marioballesteros91/" target="_blank" rel="noreferrer">LinkedIn</a></p>
                 <p>maballesteros@protonmail.com</p>
