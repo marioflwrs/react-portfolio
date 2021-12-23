9beb398667b48f61d001f977dd4f59055cdc0f54
wawamario <mariomuloz@gmail.com>
2021-12-23 17:50:56 -0500

    changes to landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,18 +1,14 @@
 import './Landing.scss';
+import Navigation from '../Navigation/Navigation';
+import React from 'react';
 
-import { Link  } from "react-router-dom";
+// import { Link  } from "react-router-dom";
 
-const Landing = () => {
-    return(
-        <div className="landing-background landing-container">
-            <div className="landing-nav-container">
-                <Link to="/work"><h1>Work</h1></Link>
-                <Link to="/about"><h1>About</h1></Link>
-                <Link to="/breaking"><h1>Breakin'</h1></Link>
-                <Link to="/contact"><h1>Contact</h1></Link>
-            </div>
-        </div>
-    );
-}
 
-export default Landing;
+export default function Landing() {
+    return (
+      <div>
+          <Navigation />
+      </div>
+    )
+  }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <div className="nav-container">
+    <h1>this is my nav</h1>
+      
+    </div>
+  )
+}
