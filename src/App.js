af83868a0f0f93f512b2f84d86387922e1e70607
wawamario <mariomuloz@gmail.com>
2021-12-23 21:39:58 -0500

    added switch with react router onto landing component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-//Components
+//Component
 import Landing from './components/Landing/Landing';
-import Breaking from './components/Breaking/Breaking';
-import Work from './components/Work/Work';
-import Contact from './components/Contact/Contact';
-import About from './components/About/About';
-
+//react-router module
 import { 
   BrowserRouter as Router,
-  Switch,
   Route
 } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Switch>
         <Route exact path="/">
           <Landing />
         </Route>
-        <Route path="/breaking">
-          <Breaking />
-        </Route>
-        <Route path="/work">
-          <Work />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-      </Switch>
     </Router>
   );  
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,44 @@
 import './navigation.scss';
 import marioprofile from '../../assets/mario-side.png';
+import { Link  } from "react-router-dom";
+import About from '../About/About';
+import Work from '../Work/Work';
+import Contact from '../Contact/Contact';
+
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 export default function Navigation() {
   return (
-    <div className="nav-container">
-      <div className="profile-container">
-        <img className="profile-picture" src={marioprofile} alt="my face" />
-        <span>Mario Ballesteros</span>
-        <span>Brookly, NY</span>
-      </div>      
-    </div>
+    <Router>
+      <div className="nav-container">
+        <div className="profile-container">
+          <img className="profile-picture" src={marioprofile} alt="my face" />
+          <span>Mario Ballesteros</span>
+          <span>🗽Brooklyn, NY</span>
+        </div>
+        <div className="links-container">
+          <Link to="/about" className="nav-item">About</Link>
+          <Link to="/work" className="nav-item">Projects</Link>
+          <Link to="/contact" className="nav-item">Contact</Link>
+        </div>      
+      </div>
+
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/work">
+          <Work />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
